6c95a8282752a3bdb0b329060bf9561bd6cc8e97
LoKyLj <kyljs3d@gmail.com>
2023-10-26 23:00:48 +0300

    New gallery, modal block, adaptive seed planted

--- assets/index.js
+++ assets/index.js
@@ -27,51 +27,151 @@ const revealByScroll = () => {
 
 window.addEventListener('scroll', revealByScroll);
 
-//This function insert iframe to html after DOM load
+if (document.querySelector('.video')) {
+        //This function insert iframe to html after DOM load
 
-window.onload = () => {
-    const video = document.querySelector('.video__container');
+    window.onload = () => {
+        const video = document.querySelector('.video__container');
 
-    video.insertAdjacentHTML('afterbegin', '<iframe class="video__container-item" src="https://www.youtube.com/embed/F1u8WIb3j7Q?enablejsapi=1&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+        video.insertAdjacentHTML('afterbegin', '<iframe class="video__container-item" src="https://www.youtube.com/embed/F1u8WIb3j7Q?enablejsapi=1&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+    }
+
+    //This fuction reveals object with '.video__container' class and blocks scrolling on the page
+
+    const revealOnClick = () => {
+        let videoItem = document.querySelector('.video__container');
+
+        videoItem.classList.remove('video__container--hide');
+
+        videoItem.classList.add('video__container--display');
+
+        document.body.style.overflow = ('hidden');
+        document.body.style.height = ('100vh');
+    }
+
+    const videoAdd = document.querySelector('.video');
+    videoAdd.addEventListener('click', revealOnClick);
+
+    //This function hides object with '.video__container--display' class
+
+    const hideOnClick = () => {
+        let hide = document.querySelector('.video__container');
+        hide.classList.add('video__container--hide');
+
+        document.body.style.overflowY = ('');
+        document.body.style.height = ('');
+    }
+
+    const videoRemove = document.querySelector('.video__container');
+    videoRemove.addEventListener('click', hideOnClick);
+
+    //This function stops YouTube playing with player hiding
+
+    function controlVideo() {
+        let iframe = document.getElementsByTagName("iframe")[0].contentWindow;
+        iframe.postMessage(
+            '{"event":"command","func":"' + 'pauseVideo' + '","args":""}',
+            "*"
+        );
+    }
+
+    //fix
+    // document.addEventListener('keydown', function (e) {
+    //     if (!document.querySelector('.video__container--hide')) {
+    //         if (e.key === "Escape") {
+    //         hideOnClick();
+    //         }
+    //     }
+    // });
+    
+    videoRemove.addEventListener('click', controlVideo);
 }
 
-//This fuction reveals object with '.video__container' class and blocks scrolling on the page
 
-const revealOnClick = () => {
-    let videoItem = document.querySelector('.video__container');
+//
 
-    videoItem.classList.remove('video__container--hide');
+if (document.querySelector('.gallery')) {
+//     const galleryItems = document.querySelectorAll('.gallery__item');
+//     const gallery = document.querySelector('.gallery');
+//     const modal = document.createElement('div');
+//     modal.classList.add('modal');
 
-    videoItem.classList.add('video__container--display');
+//     let currentIndex = 0;
 
-    document.body.style.overflow = ('hidden');
-    document.body.style.height = ('100vh');
-}
+//     const openModal = (index) => {
+//         currentIndex = index;
+//         modal.innerHTML = '';
+//         const content = galleryItems[currentIndex].cloneNode(true);
+//         modal.appendChild(content);
 
-const videoAdd = document.querySelector('.video');
-videoAdd.addEventListener('click', revealOnClick);
+//         gallery.appendChild(modal);
+// }   
 
-//This function hides object with '.video__container--display' class
+//     galleryItems.forEach((item, index) => {
+//         item.addEventListener('click', () => openModal(index));
+//     });
 
-const hideOnClick = () => {
-    let hide = document.querySelector('.video__container');
-    hide.classList.add('video__container--hide');
+    document.addEventListener('DOMContentLoaded', function () {
+        const galleryItems = document.querySelectorAll('.gallery__item');
+        const modal = document.querySelector('.modal');
+        const modalImage = document.querySelector('.modal-image');
+        const modalVideo = document.querySelector('.modal-video');
+        const closeBtn = document.querySelector('.closeButton');
+        const prevBtn = document.querySelector('.prev');
+        const nextBtn = document.querySelector('.next');
+        let currentImageIndex = -1;
 
-    document.body.style.overflowY = ('');
-    document.body.style.height = ('');
-}
+        function openModal(index) {
+            if (index < 0) {
+                index = galleryItems.length - 1;
+            } else if (index >= galleryItems.length) {
+                index = 0;
+            }
 
-const videoRemove = document.querySelector('.video__container');
-videoRemove.addEventListener('click', hideOnClick);
+            const content = galleryItems[index].querySelector('img, video');
+            if (content.tagName === 'IMG') {
+                modalImage.src = content.src;
+                modalImage.style.display = 'block';
+                modalVideo.style.display = 'none';
+            } else if (content.tagName === 'VIDEO') {
+                modalVideo.querySelector('source').src = content.querySelector('source').src;
+                modalImage.style.display = 'none';
+                modalVideo.style.display = 'block';
+            }
+            modal.style.display = 'block';
+            currentImageIndex = index;
+        }
 
-//This function stops YouTube playing with player hiding
+        function closeModal() {
+            modal.style.display = 'none';
+        }
 
-function controlVideo() {
-    let iframe = document.getElementsByTagName("iframe")[0].contentWindow;
-    iframe.postMessage(
-        '{"event":"command","func":"' + 'pauseVideo' + '","args":""}',
-        "*"
-    );
-}
+        function showPrevious() {
+            openModal(currentImageIndex - 1);
+        }
 
-videoRemove.addEventListener('click', controlVideo);
+        function showNext() {
+            openModal(currentImageIndex + 1);
+        }
+
+        galleryItems.forEach(function (item, index) {
+            item.addEventListener('click', function () {
+                openModal(index);
+            });
+        });
+
+        closeBtn.addEventListener('click', closeModal);
+        prevBtn.addEventListener('click', showPrevious);
+        nextBtn.addEventListener('click', showNext);
+
+        document.addEventListener('keydown', function (e) {
+            if (e.key === "Escape") {
+                closeModal();
+            } else if (e.key === "ArrowLeft") {
+                showPrevious();
+            } else if (e.key === "ArrowRight") {
+                showNext();
+            }
+        });
+    });
+};
